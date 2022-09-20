f9023e3b954ee91b80cbf6ef0ee6048a0d76ab22
Tobias Martins <tobiasaires@gmail.com>
2022-09-20 10:40:34 -0300

    Initial Commit

---
+++ packages/container/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import MarketingApp from './components/MarketingApp'
+
+export default () => {
+  return (
+    <>
+      <h1>Hi</h1>
+      <MarketingApp />
+    </>
+  )
+}
