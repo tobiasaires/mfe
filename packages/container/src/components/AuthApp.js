1f677f40b51d65900abdf2aab9e5fb22396a9767
Tobias Martins <tobiasaires@gmail.com>
2022-09-21 16:29:19 -0300

    Finalização

--- packages/container/src/components/AuthApp.js
+++ packages/container/src/components/AuthApp.js
@@ -1,22 +1,24 @@
-import { mount } from 'marketing/MarketingApp'
+import { mount } from 'auth/AuthApp'
 import React, { useRef, useEffect } from 'react'
 import { useHistory } from 'react-router-dom'
 
-export default () => {
+export default ({ onSignIn }) => {
   const ref = useRef(null)
   const history = useHistory()
 
   useEffect(() => {
     const { onParentNavigate } = mount(ref.current, {
       initialPath: history.location.pathname,
-      onNavigate: ({ pathname: nextPathName }) => {
+      onNavigate: ({ pathname: nextPathname }) => {
         const { pathname } = history.location
 
-        if (pathname !== nextPathName) {
-          history.push(nextPathName)
+        if (pathname !== nextPathname) {
+          history.push(nextPathname)
         }
       },
+      onSignIn,
     })
+
     history.listen(onParentNavigate)
   }, [])
 
